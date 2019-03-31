559184f251f7eafea7cc1f27b3c7106ed5f4687e
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 05:54:20 +0430

    Update ConvertCalendar.c

--- source/ConvertCalendar.c
+++ source/ConvertCalendar.c
@@ -12,5 +12,14 @@
 
 date gregorian2jalali(date input) {
 	date result;
+	int array[]={0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+	if(input.year<=1600) {
+		input.year-=621;
+		result.year=0;
+	}
+	else {
+		input.year-=1600;
+		result.year=979;
+	}
 	return result;
 }
