bce1b0af64780edd93f4aff8ba71749646837dbb
Max Base <MaxBaseCode@Gmail.Com>
2019-03-31 05:50:54 +0430

    Update ConvertCalendar.h

--- source/ConvertCalendar.h
+++ source/ConvertCalendar.h
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : ConvertCalendar.h
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-31
+* @Released under : https://github.com/BaseMax/ConvertCalendar/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/ConvertCalendar
+*
+**/
